38164163befd7d563f7e14b3dfd1c02abf4811a4
akandiew <andrewkandiew@Andrews-MBP.home>
2020-05-08 23:38:39 -0400

    all instructions

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Impossible Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Super Bacon Cheese Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Dagwood Burger", false);
